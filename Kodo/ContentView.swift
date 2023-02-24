5589dc460aa1e5da1e04023e2f7365a93d4307c5
Razan <rzoOonfahd@hotmail.com>
2023-02-24 19:04:23 +0300

    Initial Commit

---
+++ Kodo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Kodo
+//
+//  Created by Razan Aldossari on 04/08/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kodo/KodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodoApp.swift
+//  Kodo
+//
+//  Created by Razan Aldossari on 04/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct KodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
